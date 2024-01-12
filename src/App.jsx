9769567ceec7dcb08dd01df479a621b2d7a774c2
LeilanNaeimi <leilan.n@gmail.com>
2024-01-12 09:42:12 +0300

    add ppl

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
 import React, { useState } from "react";
 import ButtonUI from "./ButtonUI";
 
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
 export default function App() {
   return (
     <div className="app">
       <div className="left">
-        <People />
+        <Peoples />
         <AddNewPeople />
       </div>
 
@@ -15,7 +48,7 @@ export default function App() {
   );
 }
 
-function People() {
+function Peoples() {
   return (
     <div className="people">
       <PeopleList />
@@ -23,29 +56,42 @@ function People() {
   );
 }
 
-function PeopleList({ name }) {
-  return <div className="peoplelist">{name}</div>;
+function PeopleList() {
+  const ppls = initialFriends;
+  return (
+    <ul className="peoplelist">
+      {ppls.map((ppl) => (
+        <People ppl={ppl} key={ppl.id} />
+      ))}
+    </ul>
+  );
+}
+
+function People({ ppl }) {
+  return (
+    <li>
+      <img src={ppl.image} alt={ppl.name} />
+      <h3>{ppl.name}</h3>
+      <button>
+        <span>Select</span>
+      </button>
+      <p>you owe sarah €{ppl.balance}</p>
+    </li>
+  );
 }
 
 function AddNewPeople() {
-  const [name, setName] = useState("");
-
   return (
     <div className="addnew">
       <div className="input-group">
         <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+        <input type="text" id="name" />
       </div>
       <div className="input-group">
         <label htmlFor="img">Image URL</label>
         <input type="text" id="img" />
       </div>
-      <button onClick={setName}>
+      <button>
         <span>Add New</span>
       </button>
     </div>
